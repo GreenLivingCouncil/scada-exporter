d04792758d9e6f6316e6653dcb78f819f85259b9
sckoo <stephen.theking@gmail.com>
2014-01-25 22:06:32 -0800

    code cleanup

--- bin/ccn.py
+++ bin/ccn.py
@@ -2,19 +2,27 @@
 import json
 import urllib2
 import datetime
+import re
 import os
 from requests import session
 
 SCADA_URL = "http://scadaweb.stanford.edu/ion/data/getRTxmlData.asp?dgm=//scadaweb/ion-ent/config/diagrams/ud/temp/amrit.dgm&node=WebReachDefaultDiagramNode"
 DASHBOARD_LOGIN_URL = "http://buildingdashboard.net/login"
+FORM_URL = "http://buildingdashboard.net/facilities/point/{form_id}/data"
 LAST_DATA_PATH = "last.json"
 NEW_DATA_PATH = "current.json"
 DT_FORMAT = "%m/%d/%Y %H:%M:%S"
+SUM_DEFS = [
+    ("CROTHERS.STERN", ("CROTHERS.STERN_BURBANK_ZAPATA_E1152", "CROTHERS.STERN_DONNER_SERRA_E1151", "CROTHERS.STERN_TWAINS_LARKINS_E1154"))
+    ]
 
 def get_csrf_token(page_text):
-    token_i = page_text.find('csrfmiddlewaretoken') + 28
-    token_end = page_text.find('\'', token_i)
-    return page_text[token_i:token_end]
+    search_start = page_text.find('csrfmiddlewaretoken')
+    search_end = page_text.find('>', search_start)
+    match = re.search(r"value='(\w*)'", page_text[search_start:search_end])
+    if not match:
+        raise Exception("csrf token could not be found.")
+    return match.group(1)
 
 def smart_post(conn, url, data):
     response = conn.get(url)
@@ -22,7 +30,7 @@ def smart_post(conn, url, data):
     return conn.post(url, data=data)
 
 def get_form_url(building_codes):
-    return ("http://buildingdashboard.net/facilities/point/%s/data" % building_codes[1])
+    return FORM_URL.format(form_id=building_codes[1])
 
 def rounded_hour(dt):
     """Returns the rounded hour of the given Datetime object."""
@@ -31,12 +39,12 @@ def rounded_hour(dt):
 def get_time_interval(last_datestring, new_datestring):
     last_date = datetime.datetime.strptime(last_datestring, DT_FORMAT)
     new_date = datetime.datetime.strptime(new_datestring, DT_FORMAT)
-    return (
-        last_date.strftime("%m/%d/%Y"),
-        rounded_hour(last_date),
-        new_date.strftime("%m/%d/%Y"),
-        rounded_hour(new_date)
-        )
+    return {
+        'localStart' :     last_date.strftime("%m/%d/%Y"),
+        'localStartTime' : rounded_hour(last_date),
+        'localEnd' :       new_date.strftime("%m/%d/%Y"),
+        'localEndTime' :   rounded_hour(new_date)
+        }
 
 def get_submission_error(page_text):
     start = page_text.find('errorlist') + 15
@@ -48,74 +56,62 @@ def get_submission_error(page_text):
     return page_text[start:end]
 
 def push_data(last_data, new_data):
-    # Get the needed values from the datestrings
-    time_interval = get_time_interval(last_data['date'], new_data['date'])
-
     # Open the dictionary for building codes
     with open('codes.json') as codes_f:
         codes = json.load(codes_f)
 
-    with session() as c:
+    # Get the needed values from the datestrings
+    time_interval = get_time_interval(last_data['date'], new_data['date'])
+
+    with session() as conn:
         # Login
-        payload = {
+        request = smart_post(conn, DASHBOARD_LOGIN_URL, {
             'username': 'sashab@stanford.edu',
             'password': 'stanfordglcccn'
-            }
-        request = smart_post(c, DASHBOARD_LOGIN_URL, data=payload)
+            })
 
         # Upload data
-        for (buildingName, dataValue) in new_data['data'].items():
+        for (building_name, new_reading) in new_data['data'].items():
             # Skip if the meter for this building had an error at last reading 
-            if buildingName not in last_data['data']:
+            # Or if a building code doesn't exist for this building.
+            if building_name not in last_data['data'] or not codes[building_name]:
                 continue
-            submit_one(c, codes[buildingName], dataValue - last_data['data'][buildingName], time_interval)
+            last_reading = last_data['data'][building_name]
+            submit_one(conn, codes[building_name], new_reading - last_reading, time_interval)
 
-def submit_one(conn, building_codes, data_val, time_interval):
-    if not building_codes:
-        return
-
-    payload = {
-        'localStart': time_interval[0],
-        'localStartTime': time_interval[1],
-        'localEnd': time_interval[2],
-        'localEndTime': time_interval[3],
-        'value': data_val
-        }
+def submit_one(conn, building_codes, value, time_interval):
+    payload = {'value' : value}
+    payload.update(time_interval)
     request = smart_post(conn, get_form_url(building_codes), payload)
+
     if u"Reading added" not in request.text:
         with open("dump.html", "w") as f:
             f.write(request.text.encode('ascii', 'ignore'))
         raise Exception("%s %s" % (repr(building_codes), get_submission_error(request.text)))
 
+class BuildingNode(object):
+    def __init__(self, xml_node):
+        self.name = xml_node.attrib['nodeName']
+        self.kwh = int(xml_node[1].attrib['v'].translate(None, ', ')) if self.name != "VIP.SCADAWEB" else 0
+        self.has_error = ('e' in xml_node[1].attrib)
+
 def pull_data():
     import xml.etree.ElementTree as ET
 
-    # Get date and create result dict
-    datestring = datetime.datetime.now().strftime(DT_FORMAT)
-
-    # Get data from SCADA page
+    # Parse data from SCADA page
     response = urllib2.urlopen(SCADA_URL)
     root = ET.fromstring(response.read())
-    # Loop through building nodes and add data to a new dict
-    data = {}
-    for buildingNode in root:
-        buildingName = buildingNode.attrib['nodeName']
-        if buildingName == "VIP.SCADAWEB":
-            continue
-        # Skip if there's an error message attribute
-        if "e" in buildingNode[1].attrib:
-            continue
-        # Sum up the Stern dorms
-        if buildingName in ["CROTHERS.STERN_BURBANK_ZAPATA_E1152", "CROTHERS.STERN_DONNER_SERRA_E1151", "CROTHERS.STERN_TWAINS_LARKINS_E1154"]:
-            if "CROTHERS.STERN" not in data:
-                data["CROTHERS.STERN"] = 0
-            data["CROTHERS.STERN"] += int(buildingNode[1].attrib['v'].translate(None, ', '))
-        # Put in data
-        data[buildingName] = int(buildingNode[1].attrib['v'].translate(None, ', '))
+    buildings = [BuildingNode(node) for node in root]
+    data = dict((building.name, building.kwh) for building in buildings
+                if not (building.name == "VIP.SCADAWEB" and building.has_error))
+
+    # Carry out definitions for combined dorms.
+    for combined, parts in SUM_DEFS:
+        data[combined] = sum(data[part] for part in parts)
 
     # Add data dict to the result
     result = {
-        "date": datestring,
+        "date": datetime.datetime.now().strftime(DT_FORMAT),
         "data": data
         }
     return result
@@ -128,8 +124,7 @@ def save_data(data_dict, dest):
 def open_data(src):
     """Thin wrapper function for returning a dict containing the contents of a JSON on disk."""
     with open(src) as src_f:
-        result = json.load(src_f)
-    return result
+        return json.load(src_f)
 
 def setup(overwrite=False):
     """If a last data file doesn't exist, create it from new data."""
